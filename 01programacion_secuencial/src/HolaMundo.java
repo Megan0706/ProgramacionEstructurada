1152fd245ad54d7a5ba0d4610ba6e881b19d0229
Megan Howard <mjraiz.com@gmail.com>
2025-01-23 14:56:13 -0600

    Logro: Primeros Programas Secuenciales

---
+++ 01programacion_secuencial/src/HolaMundo.java
@@ -0,0 +1,10 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+
+        double e1= 2+5*3.0/2.0+5*(2+2);
+        System.out.println(e1);
+
+
+    }
+}
